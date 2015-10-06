d2571adb70dff9f276dd17e61e166ba7323667b1
sanjjay <sanjaydutt01@hotmail.com>
2015-10-06 13:01:08 +0100

    Update
    Update

--- service.playit.browser-extensions/firefox/source/data/playIt-panel.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("save").onclick=function(){
-    data = {
-        'serviceAddress':document.getElementById('serviceAddress').value,
-        'servicePort':document.getElementById('servicePort').value
-    }
-    console.error(data.serviceAddress)
-    self.port.emit("save", data);
-}
